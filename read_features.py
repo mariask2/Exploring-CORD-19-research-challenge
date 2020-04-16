39e492c680981a2fab8f1afafa1436689072fbcd
mariask2 <mariask@kth.se>
2020-04-16 09:21:15 +0200

    Using a model on new data

--- read_features.py
+++ read_features.py
@@ -1,6 +1,12 @@
 from gensim.models import KeyedVectors
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegressionCV
+from nltk.tokenize import TweetTokenizer
+from joblib import dump, load
+import glob
+import json
+import csv
+import os.path
 import numpy as np
 
 OUTSIDE_LABEL = "O"
@@ -8,14 +14,6 @@ INSIDE_LABEL = "I"
 WINDOW_SIZE = 5
 VECTOR_LENGTH = 200
 default_vector = np.array([0.0] * VECTOR_LENGTH)
-
-#X, y = load_iris(return_X_y=True)
-#print(X)
-#print(y)
-#exit(1)
-#clf = LogisticRegression(random_state=0, max_iter=1000).fit(X, y)
-
-#print(clf.predict(X[:2, :]))
 
 
 def get_vector_for_word(word, word2vec_model):
@@ -76,22 +74,99 @@ def get_training_labels_vec(training_labels):
             labels.append(1)
     return labels
 
-#
-#res = get_vectors(["diabetes", "mellitus", "seenrnrnrnqlsowk"], word2vec_model)
-#for el in res:
-#    print(el)
-#    print(el.shape)
-
-training_data, training_labels = read_data("manually_annotated_data.txt")
-
 word2vec_model = KeyedVectors.load_word2vec_format('/Users/marsk757/wordspaces/pubmed2018_w2v_200D.bin', binary=True)
-y = get_training_labels_vec(training_labels)
-X = get_vectors(training_data, word2vec_model)
-clf = LogisticRegressionCV(random_state=0, max_iter=1000, cv=5, class_weight="balanced", n_jobs=4).fit(X, y)
-prediction_prob = clf.predict_proba(X)
-predictions = clf.predict(X)
+model_file_name = 'risk_mention_model.joblib'
 
-for x, y, predicted_prob, predicted in zip(training_data, y, prediction_prob, predictions):
-    print(x, y, predicted, predicted_prob)
+if not os.path.isfile(model_file_name):
+    # If no model has been trained before
+    training_data, training_labels = read_data("manually_annotated_data.txt")
 
-print(clf.classes_)
+    
+    y = get_training_labels_vec(training_labels)
+    X = get_vectors(training_data, word2vec_model)
+    clf = LogisticRegressionCV(random_state=0, max_iter=1000, cv=5, class_weight="balanced", n_jobs=8).fit(X, y)
+
+    dump(clf, model_file_name)
+
+    clf_loaded = load(model_file_name)
+
+    prediction_prob = clf_loaded.predict_proba(X)
+    predictions = clf_loaded.predict(X)
+
+    for x, y, predicted_prob, predicted in zip(training_data, y, prediction_prob, predictions):
+        print(x, y, predicted, predicted_prob)
+
+# Use the model
+clf_loaded = load(model_file_name)
+
+CONTENT_DIR = "../CORD-19-research-challenge"
+
+headings_to_exclude_set = set()
+with open("headings_to_exclude.txt") as f:
+    for line in f:
+        headings_to_exclude_set.add(line.strip())
+
+meta_data_dict = {}
+with open(os.path.join(CONTENT_DIR, 'metadata.csv')) as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        id = row[0].strip()
+        if ";" in id:
+            id_list = [x.strip() for x in id.split(";")]
+        else:
+            id_list = [id]
+        for id_el in id_list:
+            meta_data_dict[id_el] = row
+
+tknzr = TweetTokenizer(preserve_case=True)
+
+file_dict = {}
+prediction_scores = [(0.0, "", "")]*20
+for dir in ["biorxiv_medrxiv"]:# , comm_use_subset", "custom_license", "noncomm_use_subset"]:
+    path = os.path.join(CONTENT_DIR, dir, dir)
+    files = glob.glob(path + "/*.json")
+    for file in files:
+        with open(file) as f:
+            file_text = []
+            plain_text_lower = ""
+            data = json.load(f)
+            paper_id = data["paper_id"].strip()
+            meta_data = []
+            if paper_id in meta_data_dict:
+                meta_data = meta_data_dict[paper_id]
+            else:
+                print(paper_id + " not in metadata")
+                print(nr_of_sections)
+                print()
+                pass
+                  
+            current_section = None
+            for el in data["body_text"]:
+                if el["section"].lower() != current_section:
+                    current_section = el["section"].lower()
+                    if current_section not in headings_to_exclude_set:
+                        text = el["text"]
+                        plain_text_lower = plain_text_lower + " " + text.lower()
+                        file_text.append(tknzr.tokenize(text))
+
+            if "covid" in plain_text_lower or "coronavirus disease 2019" \
+                in plain_text_lower or "sars-cov-2" in plain_text_lower \
+                or "2019-nCoV" in plain_text_lower:
+                
+                for paragraph in file_text:
+                    X = get_vectors(paragraph, word2vec_model)
+                    prediction_prob = clf_loaded.predict_proba(X)
+                    predictions = clf_loaded.predict(X)
+                    if 1 in predictions:
+                        for x,  predicted_prob, predicted in zip(paragraph, prediction_prob, predictions):
+                            if predicted == 1:
+                                prediction_scores.append((np.amax(predicted_prob), " ".join(paragraph), x))
+                                prediction_scores.sort(reverse=True)
+                                prediction_scores = prediction_scores[:-1]
+                            #print(x, predicted, predicted_prob)
+                            #print(prediction_scores)
+                    file_dict[paper_id] = file_text
+
+  
+print(prediction_scores)
+    
